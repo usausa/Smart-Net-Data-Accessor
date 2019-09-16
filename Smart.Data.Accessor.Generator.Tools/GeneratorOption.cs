f49a31d1d4cf3ef6aabb59a2151ac018d38e8e22
usausa <machi.pon@gmail.com>
2019-09-16 22:09:57 +0900

    Update version to 0.9.1

--- Smart.Data.Accessor.Generator.Tools/GeneratorOption.cs
+++ Smart.Data.Accessor.Generator.Tools/GeneratorOption.cs
@@ -6,14 +6,16 @@ namespace Smart.Data.Accessor.Generator
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Ignore")]
     public sealed class GeneratorOption : IGeneratorOption
     {
-        private readonly Dictionary<string, string> values;
+        private readonly Dictionary<string, string> values = new Dictionary<string, string>();
+
+        public IEnumerable<string> Keys { get => values.Keys; }
 
         public GeneratorOption(string parameter)
         {
-            values = parameter.Split(';')
-                .Select(x => x.Split('='))
-                .Where(x => x.Length == 2)
-                .ToDictionary(x => x[0].Trim(), x => x[1].Trim());
+            foreach (var pair in parameter.Split(';').Select(x => x.Split('=')).Where(x => x.Length == 2))
+            {
+                values[pair[0].Trim()] = pair[1].Trim();
+            }
         }
 
         public string GetValue(string key) => values.TryGetValue(key, out var value) ? value : string.Empty;
