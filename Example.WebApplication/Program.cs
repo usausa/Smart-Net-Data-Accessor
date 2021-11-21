a90a6d77d1a330acc4cfe3a73068e9eeaf20ef43
usausa <machi.pon@gmail.com>
2021-11-21 18:36:08 +0900

    Update sample

--- Example.WebApplication/Program.cs
+++ Example.WebApplication/Program.cs
@@ -1,19 +1,50 @@
-namespace Example.WebApplication;
+#pragma warning disable CA1812
+using Microsoft.Data.Sqlite;
 
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Smart.Data;
+using Smart.Data.Accessor.Extensions.DependencyInjection;
+using Smart.Data.Mapper;
 
-public static class Program
+// Configure builder
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Data access
+builder.Services.AddSingleton<IDbProvider>(new DelegateDbProvider(() => new SqliteConnection("Data Source=test.db")));
+builder.Services.AddDataAccessor();
+
+// Configure
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Data initialize
+var dbProvider = app.Services.GetRequiredService<IDbProvider>();
+dbProvider.Using(con =>
+{
+    con.Execute("CREATE TABLE IF NOT EXISTS Data (Id int PRIMARY KEY, Name text, Type text)");
+    con.Execute("DELETE FROM Data");
+    con.Execute("INSERT INTO Data (Id, Name, Type) VALUES (1, 'Data-1', 'A')");
+    con.Execute("INSERT INTO Data (Id, Name, Type) VALUES (2, 'Data-2', 'B')");
+    con.Execute("INSERT INTO Data (Id, Name, Type) VALUES (3, 'Data-3', 'A')");
+});
+
+app.Run();
--- Example.WebApplication2/Program.cs
+++ Example.WebApplication2/Program.cs
@@ -1,22 +1,83 @@
-namespace Example.WebApplication2;
+#pragma warning disable CA1812
+using Example.WebApplication2;
 
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Microsoft.Data.Sqlite;
 
+using Smart.Data;
+using Smart.Data.Accessor.Extensions.DependencyInjection;
+using Smart.Data.Accessor.Resolver;
+using Smart.Data.Mapper;
 using Smart.Resolver;
 
-public static class Program
-{
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+// Configure builder
+var builder = WebApplication.CreateBuilder(args);
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .UseServiceProviderFactory(new SmartServiceProviderFactory())
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+// Custom service provider
+builder.Host.UseServiceProviderFactory(new SmartServiceProviderFactory());
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Data access
+builder.Services.AddSingleton<IDbProvider>(new DelegateDbProvider(() => new SqliteConnection("Data Source=test.db")));
+builder.Services.AddDataAccessor();
+
+// Custom service provider
+builder.Host.ConfigureContainer<ResolverConfig>(config =>
+{
+    config.UseDataAccessor();
+    config
+        .Bind<IDbProvider>()
+        .ToConstant(new DelegateDbProvider(() => new SqliteConnection("Data Source=primary.db")))
+        .Named(DataSource.Primary);
+    config
+        .Bind<IDbProvider>()
+        .ToConstant(new DelegateDbProvider(() => new SqliteConnection("Data Source=secondary.db")))
+        .Named(DataSource.Secondary);
+});
+
+// Configure
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Data initialize
+var selector = app.Services.GetRequiredService<IDbProviderSelector>();
+
+var provider1 = selector.GetProvider(DataSource.Primary);
+provider1.Using(con =>
+{
+    con.Execute("CREATE TABLE IF NOT EXISTS Data (Id int PRIMARY KEY, Name text, Type text)");
+    con.Execute("DELETE FROM Data");
+    con.Execute("INSERT INTO Data (Id, Name, Type) VALUES (1, 'Primary-1', 'A')");
+    con.Execute("INSERT INTO Data (Id, Name, Type) VALUES (2, 'Primary-2', 'B')");
+    con.Execute("INSERT INTO Data (Id, Name, Type) VALUES (3, 'Primary-3', 'A')");
+});
+
+var provider2 = selector.GetProvider(DataSource.Secondary);
+provider2.Using(con =>
+{
+    con.Execute("CREATE TABLE IF NOT EXISTS Data (Id int PRIMARY KEY, Name text, Type text)");
+    con.Execute("DELETE FROM Data");
+    con.Execute("INSERT INTO Data (Id, Name, Type) VALUES (1, 'Secondary-1', 'B')");
+    con.Execute("INSERT INTO Data (Id, Name, Type) VALUES (2, 'Secondary-2', 'A')");
+    con.Execute("INSERT INTO Data (Id, Name, Type) VALUES (3, 'Secondary-3', 'B')");
+});
+
+app.Run();
