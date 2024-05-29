fd8ead345bef70580c35d6b4aafcede1d5ddd1b8
usausa <machi.pon@gmail.com>
2024-05-29 09:16:28 +0900

    Fix escape

--- Smart.Data.Accessor/Data/Accessor/Nodes/SqlNode.cs
+++ Smart.Data.Accessor/Data/Accessor/Nodes/SqlNode.cs
@@ -6,7 +6,7 @@ public sealed class SqlNode : INode
 
     public SqlNode(string sql)
     {
-        Sql = sql.Replace(@"\", @"\\", StringComparison.Ordinal);
+        Sql = sql;
     }
 
     public void Visit(INodeVisitor visitor) => visitor.Visit(this);
