13df70f13ba9c722e386e079a7f63bd36b9f4635
usausa <machi.pon@gmail.com>
2026-06-06 19:32:30 +0900

    Update

---
+++ Smart.Data.Accessor.Builders.Postgres.Generator/Assembly.cs
@@ -0,0 +1,8 @@
+// Polyfill for record types on netstandard2.0
+#pragma warning disable IDE0130
+#pragma warning disable CA1812
+namespace System.Runtime.CompilerServices;
+
+internal sealed class IsExternalInit
+{
+}
